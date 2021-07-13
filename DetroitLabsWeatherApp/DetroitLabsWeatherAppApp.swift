ae7c8c8896c721f0cfb0457fb81cd3b9e328d69b
Austin Evans <evansau1@msu.edu>
2021-07-12 21:04:28 -0400

    Initial Commit

---
+++ DetroitLabsWeatherApp/DetroitLabsWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DetroitLabsWeatherAppApp.swift
+//  DetroitLabsWeatherApp
+//
+//  Created by Austin Evans on 7/12/21.
+//
+
+import SwiftUI
+
+@main
+struct DetroitLabsWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
