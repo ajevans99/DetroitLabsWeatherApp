2026af4f33b0daca03c841fe9c991864f6e35d30
Austin Evans <evansau1@msu.edu>
2021-07-16 10:44:55 -0400

    Create seperate view for Forecast Day

--- DetroitLabsWeatherApp/Views/ForecastView.swift
+++ DetroitLabsWeatherApp/Views/ForecastView.swift
@@ -29,19 +29,7 @@ struct ForecastView: View {
                         .padding()
                     ForEach(forecast.sortedDateKeys, id: \.self) { day in
                         Text("\(forecast.conditionsByDay[day]![0].date, style: .date)")
-                        HStack {
-                            ForEach(forecast.conditionsByDay[day] ?? []) { conditions in
-                                VStack {
-                                    AsyncImage(url: Endpoint.weatherIcon(for: conditions.icon), placeholder: { ProgressView() }, image: { uiImage in Image(uiImage: uiImage).resizable() })
-                                        .frame(width: 20, height: 20)
-                                        .scaleEffect(1.7)
-                                    Text("\(conditions.conditions.temp, specifier: "%.0f")")
-                                        .font(.system(size: 20, weight: .bold, design: .rounded))
-                                    Text("\(conditions.date, style: .time)")
-                                        .font(.system(size: 7, weight: .bold, design: .monospaced))
-                                }
-                            }
-                        }
+                        ForecastDayView(allConditions: forecast.conditionsByDay[day] ?? [])
                         Divider()
                             .padding()
                     }
@@ -52,5 +40,25 @@ struct ForecastView: View {
         .frame(maxWidth: .infinity, minHeight: 300)
         .padding()
         .background(Color.green.cornerRadius(32).shadow(radius: 8))
+    }
+}
+
+struct ForecastDayView: View {
+    let allConditions: [Forecast.Conditions]
+
+    var body: some View {
+        HStack {
+            ForEach(allConditions) { conditions in
+                VStack {
+                    AsyncImage(url: Endpoint.weatherIcon(for: conditions.icon), placeholder: { ProgressView() }, image: { uiImage in Image(uiImage: uiImage).resizable() })
+                        .frame(width: 20, height: 20)
+                        .scaleEffect(1.7)
+                    Text("\(conditions.conditions.temp, specifier: "%.0f")")
+                        .font(.system(size: 20, weight: .bold, design: .rounded))
+                    Text("\(conditions.date, style: .time)")
+                        .font(.system(size: 7, weight: .bold, design: .monospaced))
+                }
+            }
+        }
     }
 }
